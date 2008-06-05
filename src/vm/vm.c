bb5b0d78dca3e3eb88da97b8e35888bc6e2f9bee
Don Mahurin <dmlist@openright.org>
2008-06-05 08:45:47 -0700

    expose vm for external use.
    This is required for "dvdbackupX", a modification of dvdbackup that finds and avoids unused/bad blocks by using
    libdvdread and libdvdnav/libdvdvm to follow title sets to find used blocks,

--- src/vm/vm.c
+++ src/vm/vm.c
@@ -260,6 +260,12 @@ int ifoOpenNewVTSI(vm_t *vm, dvd_reader_t *dvd, int vtsN) {
     return 1; /*  We alread have it */
   }
 
+  /* for the case of external use of the vm, dvd may be controlled externally */
+  if(dvd == NULL) {
+    (vm->state).vtsN = vtsN;
+    return 1;
+  }
+
   if(vm->vtsi != NULL)
     ifoClose(vm->vtsi);
 
@@ -1154,6 +1160,8 @@ static int process_command(vm_t *vm, link_t link_values) {
     vm_print_current_domain_state(vm);
     Log3(vm, "After printout ends.");
 #endif
+
+    if(vm->dvd == NULL) break; /* Only process one command if vm is used stand-alone */
 
   }
 
